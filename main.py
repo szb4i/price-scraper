397ca30662170d36ab533cd90f8a1245fb1c207a
szbai <szabolcs.nagy.hu@gmail.com>
2022-05-11 17:56:44 +0200

    no credentials needed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from binance.client import Client
-import credentials
 from web_socket import WebSocket
 
 if __name__ == "__main__":
-    client = Client(credentials.getBinanceKey(), credentials.getBinanceSecretKey())
     web_socket = WebSocket()
     web_socket.run_forever()
